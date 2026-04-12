f8eb5ec57eb8d95374721cc9703be9bac2bf2f8b
Tony Briza <acbriza@gmail.com>
2026-04-12 20:28:02 +0800

    create optreport.py

---
+++ module/optreport.py
@@ -0,0 +1,137 @@
+"""
+    Produce feature and model selection reports based on study from selection.ipynb
+"""
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+
+import joblib
+import json
+from pathlib import Path
+import shutil
+from datetime import datetime
+from tqdm import tqdm
+
+from sklearn.metrics import roc_curve, confusion_matrix, roc_auc_score
+from catboost import CatBoostClassifier
+from skopt.space import Integer, Real
+
+
+import sys 
+sys.path.append('..')  
+import warnings
+warnings.filterwarnings('ignore')
+
+from dataload import DPN_data
+import ymlconfig
+
+from utils2 import optimization as hpo
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python optreports.py <config file> <overwrite>")
+        sys.exit(1)
+
+    if len(sys.argv) == 2:
+        overwrite_optimization_reports = False
+    else:
+        overwrite_optimization_reports = sys.argv[2]=='overwrite'
+    
+    config_path = Path(r'experiments')
+
+    # sample config_filename = bin_opt_final.yml
+    config_filename = sys.argv[1]
+
+    # ## Read Config File    
+    current_file = Path(__file__).resolve() # Get the absolute path of the current file
+    script_dir = current_file.parent # Get the directory containing the file
+
+    config_path = Path(script_dir /'experiments')
+    config_dict = ymlconfig.load_config(config_path / config_filename)
+    config = ymlconfig.dict_to_namespace(config_dict)
+    print(config)
+
+    # #### Set output directory
+    outputdir = config_path /  config.experiment.classification_type /  config.experiment.stage / config.experiment.tag 
+    outputdir.mkdir(parents=True, exist_ok=True)
+
+    # #### Copy config file to output directory
+    source = config_path / config_filename
+    destination = outputdir / config_filename
+    shutil.copy(source, destination)
+
+    # ## Data Loading
+    D = DPN_data(config.data.dataset_path[3:])
+    D.load(classification=config.experiment.classification_type)
+    dfdpn = D.df
+    data_cols = dfdpn.drop(D.non_data_cols, axis=1, errors="ignore").columns
+    no_ncs_datacols = [c for c in data_cols if c not in D.neuro_cols]
+    X = dfdpn[no_ncs_datacols]
+    y = dfdpn['Confirmed_Binary_DPN']
+    dfXy = pd.concat([X, y], axis=1)    
+
+
+    def param_space_fn(trial):
+        return  {
+            "iterations": trial.suggest_int(
+                "iterations", 
+                config.param_space.iterations.min, 
+                config.param_space.iterations.max),
+            "depth": trial.suggest_int(
+                "depth", 
+                config.param_space.depth.min, 
+                config.param_space.depth.max),
+            "learning_rate": trial.suggest_float(
+                "learning_rate", 
+                config.param_space.learning_rate.min, 
+                config.param_space.learning_rate.max, 
+                log=True),
+            "l2_leaf_reg": trial.suggest_int(
+                "l2_leaf_reg", 
+                config.param_space.l2_leaf_reg.min, 
+                config.param_space.l2_leaf_reg.max),
+        }    
+    
+    start_time = datetime.now()    
+    print(f'Running repeated crossvalidation, started at: ', start_time.strftime("%H:%M:%S"))
+    opt_results = hpo.nested_cv_youden_optuna(
+        X=X.values,
+        y=y.values,
+        model_class=hpo.model_class[config.model.name],   # class, not an instance
+        param_space_fn=param_space_fn,
+        n_splits_outer=config.optimization.k_splits_outer,
+        n_repeats_outer=config.optimization.n_repeats_outer,
+        n_splits_inner=config.optimization.k_splits_inner,
+        n_iter=config.optimization.n_iter,   # Optuna trials per outer fold
+        random_state=config.experiment.random_seed,
+        savedir=outputdir, 
+        overwrite=overwrite_optimization_reports,
+    )
+
+    end_time = datetime.now()
+    elapsed = end_time - start_time
+    print(f'Running repeated crossvalidation, took: {elapsed.total_seconds()/60:.2f}, ended at: ',  start_time.strftime("%H:%M:%S"))
+
+    # ### Calculate Confidence Interval 
+    opt_ci  = hpo.mean_confidence_interval(opt_results, config)
+
+    opt_results_summary = {
+        'youden': f'{opt_results["youden_mean"]:.3f} +/ {opt_results["youden_std"]:.3f}',
+        'youden ci': f'{opt_ci["youden"]["ci_lower"]:.3f} - {opt_ci["youden"]["ci_upper"]:.3f}', 
+        'roc_auc': f'{opt_results["roc_auc_mean"]:.3f} +/ {opt_results["roc_auc_std"]:.3f}',
+        'roc_auc ci': f'{opt_ci["roc_auc"]["ci_lower"]:.3f} - {opt_ci["roc_auc"]["ci_upper"]:.3f}', 
+        'threshold': f'{opt_results["threshold_mean"]:.3f} +/ {opt_results["threshold_std"]:.3f}',
+        'specificity mean': f'{opt_results["specificity_mean"]:.3f}',
+        'sensitivity mean': f'{opt_results["sensitivity_mean"]:.3f}',
+    }
+
+    opt_results_df = pd.DataFrame(opt_results_summary, index=['value']).T
+
+    with open(outputdir / "optimization_results.json", "w") as f:
+        json.dump(opt_results, f, indent=4)
+
+    opt_results_df.to_csv(outputdir / "optimization_results_summary.csv")
+
+if __name__ == "__main__":
+    main()
